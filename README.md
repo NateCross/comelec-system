e7eb71e7a28c7ae404873751c10e90e996604009
Nathan Angelo B. Cruz <jonathan.a.b.cross@gmail.com>
2023-06-03 01:08:45 +0800

    Clean up CLIENT main and routes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Test system designed for the following setup:
+This system has the following tech stack:
 
 - React as frontend (web)
 - React Native as frontend (mobile)
@@ -6,15 +6,19 @@ Test system designed for the following setup:
 
 React and React Native interface with Laravel through APIs. The endpoints are to be specified.
 
-The React was bootstrapped with Vite.js.
-
 # Setup
 
 ## Client
 
+The client portion is handled with [Vite](https://vitejs.dev/guide/), but it is likely not going to be relevant unless something goes terribly wrong.
+
 - Run `npm i`
 - Copy the `.env.example` file to `.env` and fill in the required entries
 - Run `npm run dev` to develop
+
+### Defining Routes
+
+
 
 ## Server
 
@@ -25,11 +29,27 @@ The React was bootstrapped with Vite.js.
 - `php artisan serve`
 - the database name is "comelec_database"
 
+## Mobile
+
+The mobile portion of this application is handled with [Expo](https://docs.expo.dev/).
+
+- Download the [Expo](https://play.google.com/store/apps/details?id=host.exp.exponent) app on your phone
+- Run `npm i`
+- Run `npm run android` to develop
+  - Note: You may want to connect your android device first
+
 # Notes
 
-- React Router: https://reactrouter.com/en/main/start/tutorial
+- [React Router](https://reactrouter.com/en/main/start/tutorial)
   - Gives this the SPA functionality
   - Lots of features. Nice to play around with. Give it a try
+  - **It is recommended to use the new [data router](https://reactrouter.com/en/main/routers/create-browser-router), so this is what has been used for the project**
+    - [A good example of implementation](https://github.com/remix-run/react-router/blob/dev/examples/data-router/src/app.tsx)
+
+- [React Native Paper](https://callstack.github.io/react-native-paper/)
+  - Component and theming library for mobile
+
+## Misc. Notes
 
 - https://www.twilio.com/blog/build-restful-api-php-laravel-sanctum
 - https://github.com/koole/react-sanctum
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 import axios from 'axios';
 import { Sanctum } from 'react-sanctum';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Register from './pages/Register.jsx';
-import Login from './pages/Login.jsx';
+import { RouterProvider } from "react-router-dom";
+
+import routes from './routes';
 
 // Allows useSanctum hook inside App
+// TODO: Find a way to not have to append api to
+// everything since sanctum/csrf-cookie does not
+// have the api route at the beginning
+// Possible fix: just make everything api, therefore
+// removing the api at the beginning since that's all
+// the Laravel server is going to be used for?
 const sanctumConfig = {
   apiUrl: import.meta.env.VITE_API_URL,
   csrfCookieRoute: 'sanctum/csrf-cookie',
@@ -24,51 +26,11 @@ const sanctumConfig = {
 // Set default for axios to allow auth support
 axios.defaults.withCredentials = true;
 
-// Define routes here
-const router = createBrowserRouter([
-  {
-    path: "/",
-
-    // App route defines the main layout used for all pages
-    // Suggested to put header and footer here
-    element: <App />,
-
-    children: [
-      {
-        path: "register",
-        element: <Register />,
-
-        // Used for handling forms. Allows dynamic updating
-        // when done this way, though the regular
-        // React way should work fine, too
-        // action: async ({params, request}) => {
-        //   const formData = Object.fromEntries(await request.formData());
-        //   const res = await axios.post({
-        //     url: `http://localhost:8000/auth/register`,
-        //     data: {
-        //       email: formData.email,
-        //       password: formData.password,
-        //       name: formData.name,
-        //     },
-        //     headers: {
-        //       'Content-Type': 'application/json',
-        //     }
-        //   });
-        //   return null;
-        // },
-      },
-      {
-        path: 'login',
-        element: <Login />
-      }
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Sanctum config={sanctumConfig}>
-      <RouterProvider router={router} />
-    </Sanctum>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root'))
+  .render(
+    <React.StrictMode>
+      <Sanctum config={sanctumConfig}>
+        <RouterProvider router={routes} />
+      </Sanctum>
+    </React.StrictMode>
+  )
