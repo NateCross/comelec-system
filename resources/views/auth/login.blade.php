93d73af926af3b34354488dc521f2046080609ec
Godwin Duliente <rualgodwin@gmail.com>
2023-06-11 11:40:17 +0800

    login view updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
-  <!-- CSS Link -->
-  @vite('resources/dist/styles.css')
-  <script src="https://kit.fontawesome.com/84e2199ce0.js" crossorigin="anonymous"></script>
-</head>
-<body>
-  <div class="login">
-    <div class="logo">
-      <span>Title</span>
-      <span>/Logo</span>
-    </div>
-    <div class="group">
 
-    </div>
-    @include('layouts.components.messages.short.short')
-    <form action="" method="" class="modify hero">
-      <div class="login-details">
-        <span class="title">Sign in</span>
-        <span class="description">Integer id ultricies risus. Donesuscipit neque lorem, eu aliquam nunc pellentesque.</span>
-      </div>
-      <div class="fields">
-        <div class="field full">
-          <label for="username">Username</label>
-          <input id="username" type="text" name="username" required autocomplete="username">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- CSS Link -->
+    @vite('resources/dist/styles.css')
+    <script src="https://kit.fontawesome.com/84e2199ce0.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="login">
+        <div class="logo">
+            <span>Title</span>
+            <span>/Logo</span>
         </div>
-        <div class="field full">
-          <label for="password">Password</label>
-          <input id="password" type="password" name="password" required autocomplete="password">
+        @include('layouts.components.messages.short.short')
+        <form action="" method="" class="modify hero">
+            <div class="login-details">
+                <span class="title">Sign in</span>
+                <span class="description">Integer id ultricies risus. Donesuscipit neque lorem, eu aliquam nunc
+                    pellentesque.</span>
+            </div>
+            <div class="fields">
+                <div class="field full">
+                    <label for="username">Username</label>
+                    <input id="username" type="text" name="username" required autocomplete="username">
+                </div>
+                <div class="field full">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" name="password" required autocomplete="password">
+                </div>
+            </div>
+            <div class="field button">
+                <button class="primary">Sign in</button>
+            </div>
+        </form>
+        <div class="inquiry">
+            <div class="description">For further questions or inquries, <br> please contact <a href=""
+                    class="contact">COMELEC.</a> </div>
         </div>
-      </div>
-      <div class="field button">
-        <button class="primary">Sign in</button>
-      </div>
-    </form>
-    @include('layouts.inc.footer')
     </div>
-  </div>
+
 </body>
+
 </html>
